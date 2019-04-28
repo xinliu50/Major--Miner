b2283dcfaf59102c00770dd0c9287365b9de4a51
Joey Ching <joeyching1@gmail.com>
2019-04-28 19:50:02 -0400

    tagging on frontend is working, connect to DB in next step

--- src/main/GamePage.js
+++ src/main/GamePage.js
@@ -11,12 +11,29 @@ import Send  from "@material-ui/icons/Send";
 import AudioAnalyser from "./AudioAnalyser";
 
 class GamePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: []
+    };
+  }
+
   handleSubmit = () => {
-    console.log(document.getElementById("tags").value);
+    const newTags = document.getElementById("tags").value.split(",");
+    const filteredTags = newTags.filter(tag => (!this.state.tags.includes(tag)));
+    const tags = this.state.tags.concat(filteredTags);
+    this.setState({ tags: tags });
     document.getElementById("tags").value = "";
   }
 
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      document.getElementById("submitButton").click();
+    }
+  }
+
   render() {
+    console.log(this.state.tags);
     return (
       <Grid container className="game-container" direction="column" alignItems="center" spacing={16}>
         <Grid item>
@@ -24,14 +41,18 @@ class GamePage extends Component {
         </Grid>
         <AudioAnalyser />
         <Grid item sm={10} md={6} lg={8}>
+          <p>{this.state.tags.map(tag => (tag + " "))}</p>
+        </Grid>
+        <Grid item sm={10} md={6} lg={8}>
           <FormControl margin="normal" fullWidth>
             <InputLabel>Input your tags...</InputLabel>
             <Input
               id="tags"
-              type="text" 
+              type="text"
+              onKeyPress={this.handleKeyPress}
               endAdornment={
                 <InputAdornment position="end">
-                  <IconButton onClick={this.handleSubmit}>
+                  <IconButton id="submitButton" onClick={this.handleSubmit}>
                     <Send />
                   </IconButton>
                 </InputAdornment>

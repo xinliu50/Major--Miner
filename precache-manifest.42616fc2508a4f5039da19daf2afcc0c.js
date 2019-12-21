c4ab6e63938814fe110f4b971c24c8ab6097e7f3
Xin Liu <xinliu253@yahoo.com>
2019-12-21 00:01:19 -0500

    Updates

---
+++ precache-manifest.42616fc2508a4f5039da19daf2afcc0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e559dc61fe76f03f92126e2f8fddf0a",
+    "url": "/xinliu50/Major--Miner/index.html"
+  },
+  {
+    "revision": "b5e04e439af22b108527",
+    "url": "/xinliu50/Major--Miner/static/css/main.a2fa6970.chunk.css"
+  },
+  {
+    "revision": "91eeb8b63b69dd4b64cc",
+    "url": "/xinliu50/Major--Miner/static/js/2.45d38c50.chunk.js"
+  },
+  {
+    "revision": "b5e04e439af22b108527",
+    "url": "/xinliu50/Major--Miner/static/js/main.31aa2c90.chunk.js"
+  },
+  {
+    "revision": "cbce68c82e1bb35d350e",
+    "url": "/xinliu50/Major--Miner/static/js/runtime-main.2b1a53fc.js"
+  }
+]);

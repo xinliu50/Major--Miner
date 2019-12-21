0155317736c012083767f28ecb5c1a6baf52d14b
Xin Liu <xinliu253@yahoo.com>
2019-12-21 17:27:27 -0500

    Updates

--- precache-manifest.eb62ea8c7bc74db41ed45587ed59d36c.js
+++ precache-manifest.eb62ea8c7bc74db41ed45587ed59d36c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "359f637ebd47466393c618f62fad8d2e",
+    "revision": "07bb800f61c5db7e75c21e79ed51b3c9",
     "url": "/Major--Miner/index.html"
   },
   {
-    "revision": "5692a779586172c6b157",
+    "revision": "ba25018085ef36b899ad",
     "url": "/Major--Miner/static/css/main.a2fa6970.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Major--Miner/static/js/2.7e2244f2.chunk.js"
   },
   {
-    "revision": "5692a779586172c6b157",
-    "url": "/Major--Miner/static/js/main.80e65b54.chunk.js"
+    "revision": "ba25018085ef36b899ad",
+    "url": "/Major--Miner/static/js/main.7fa76159.chunk.js"
   },
   {
     "revision": "ea06311fe1d5f26be47e",

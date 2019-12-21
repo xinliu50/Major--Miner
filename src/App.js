fd0a2ef7782468acdb571d54ed833da65f959f1e
Xin Liu <xinliu253@yahoo.com>
2019-12-21 17:23:05 -0500

    debug3

--- src/App.js
+++ src/App.js
@@ -105,7 +105,12 @@ class App extends Component {
             <main>
               
 
-              <Route exact path="/" component={LandingPage}/>
+              <Route exact path="/" component={props => (
+                  <LandingPage
+                    {...props}
+                    authenticated={this.state.authenticated}
+                  />
+                )}/>
               <Route path="/info/faq" component={FaqPage} />
               <Route path="/info/contact" component={ContactPage} />
               <Route path="/info/privacy" component={PrivacyPage} />

9129912ec813d04f9c064a2688eb4a412e137632
Xin Liu <xinliu253@yahoo.com>
2020-05-15 15:22:40 -0400

    optimiza for summary page

--- src/base.js
+++ src/base.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase/app';
-//import 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions'
@@ -12,8 +11,18 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 });
-if(window.location.hostname === 'localhost'){
-  console.log("this is localhost")
-  firebase.functions().useFunctionsEmulator('http://localhost:5001');
-}
+
+/*Originaly Google is not allow user to access cloud function from Localhost, inorder to use "CallCloudFunctionForOtherTags()" 
+function from Summary Page, we can use local emulator instead.
+Have to set up Firebase emulator then run "firebase emulators:start" or "firebase serve" command 
+rebuild your project: "npm run build" in you root directory (everytime you have some changes in the code) 
+before you run "firebase emulators:start" or "firebase serve" */
+
+//Uncommon below code when you running you application locally.
+//below set up is ONLY for Summary Page displaying other's tags
+
+// if(window.location.hostname === 'localhost'){
+//   console.log("this is localhost")
+//   firebase.functions().useFunctionsEmulator('http://localhost:5001');
+// }
 export default app;
